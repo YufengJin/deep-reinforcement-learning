df1835d3be2c4a8d6adb58b90f622d446bc3ab79
Yufeng Jin <jyf931118@gmail.com>
2021-03-19 08:51:10 +0100

    add ddpg python file

--- ddpg-bipedal/ddpg_agent.py
+++ ddpg-bipedal/ddpg_agent.py
@@ -174,6 +174,7 @@ class ReplayBuffer:
     
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
+
         experiences = random.sample(self.memory, k=self.batch_size)
 
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
